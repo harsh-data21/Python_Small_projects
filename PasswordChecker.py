d075c5b60376fae4ff2f94945d9e2ca8cfed3d83
bughunt21 <142666898+bughunt21@users.noreply.github.com>
2026-09-09 13:35:11 +0530

    PaswordChecker

---
+++ PasswordChecker.py
@@ -0,0 +1,13 @@
+""" 
+Day 5/75
+     ...# Password checker #...
+
+"""
+password = input("Enter your Password:")
+
+if password == "Python789":
+   print("Access Granted ")
+   
+else:
+   print("Access Denied")
+                  
