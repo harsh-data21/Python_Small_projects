8424179f4b183de9a7987c13783129d5d8c91797
bughunt21 <142666898+bughunt21@users.noreply.github.com>
2026-09-11 22:34:44 +0530

    MathsTable

---
+++ mathstable.py
@@ -0,0 +1,12 @@
+"""
+8/75
+MathsTable
+"""
+num1 = int(input("Enter no 1:"))
+num2 = int(input("Enter no 2:"))
+num3 = int(input("Enter no 3:"))
+
+i = 1
+while i <= 10:
+   print(i*num1, num2*i, num3*i)
+   i += 1
