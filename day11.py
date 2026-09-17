181764adfd71d2991ddc7d39ddff94774b4f1549
bughunt21 <142666898+bughunt21@users.noreply.github.com>
2026-09-17 22:18:50 +0530

    day 14

--- day11.py
+++ day11.py
@@ -1,5 +1,20 @@
 """
 Day 11/75
 
-
 """
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your Grade is:", grade)
